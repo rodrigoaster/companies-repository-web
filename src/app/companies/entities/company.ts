d16fd65f15555b0dd00240116c6327057e278c57
Rodrigo Aster <rodrigowebx@gmail.com>
2023-03-13 17:24:09 -0300

    Company: create entity.

---
+++ src/app/companies/entities/company.ts
@@ -0,0 +1,10 @@
+export interface Company {
+  _id: string;
+  name: string;
+  cnpj: string;
+  patrimony_value: number;
+  micro_enterprise: string;
+  social_registration: string;
+  name_partner: string;
+  cpf_partner: string;
+}
